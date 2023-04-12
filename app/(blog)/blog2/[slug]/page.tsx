d9408aa3e63de7bd958fd7a9e291c8c778a7bdd4
Masato Ota <heiwa4126@gmail.com>
2023-04-12 19:00:30 +0900

    update

--- app/(blog)/blog2/[slug]/page.tsx
+++ app/(blog)/blog2/[slug]/page.tsx
@@ -1,7 +1,8 @@
-import { Post, getNextId, getPost, getPreviousId } from "@/lib/blog";
+import { Post, getNextId, getPost, getPostIds, getPreviousId } from "@/lib/blog";
 import Link from "next/link";
 import { notFound } from "next/navigation";
 import { Metadata } from "next/types";
+import { Suspense } from "react";
 
 function genTitle(post: Post): string {
   return `${post.title} | blog2 entry`;
@@ -15,13 +16,37 @@ export async function generateMetadata({ params }: { params: { slug: string } })
   return { title: genTitle(post) };
 }
 
-async function Nav({ Id }: { Id: string | undefined }) {
-  if (!Id) return <></>;
-
+async function Nav({ Id, Label }: { Id: string | undefined; Label: string }) {
+  if (Id == null) return <></>;
   const post = await getPost(Id);
   return (
+    <div>
+      {Label} : <Link href={`/blog2/${Id}`}>{post?.title}</Link>
+    </div>
+  );
+}
+
+async function PrevNext({ Id }: { Id: string }) {
+  return (
+    <div>
+      {/* @ts-expect-error Async Server Component */}
+      <Nav Id={await getNextId(Id)} Label="1つ新しい記事" />
+      {/* @ts-expect-error Async Server Component */}
+      <Nav Id={await getPreviousId(Id)} Label="1つ古い記事" />
+    </div>
+  );
+}
+
+async function Content({ Id }: { Id: string }) {
+  const post = await getPost(Id);
+  if (!post) {
+    return notFound();
+  }
+  return (
     <>
-      <Link href={`/blog2/${Id}`}>{post?.title}</Link>
+      <h1>{post.title}</h1>
+      <p>{post.date}</p>
+      <p>{post.content}</p>
     </>
   );
 }
@@ -32,20 +57,25 @@ export default async function Page({ params }: { params: { slug: string } }) {
   if (!post) {
     return notFound();
   }
-  const nextId = await getNextId(id);
-  const prevId = await getPreviousId(id);
 
   return (
     <main>
-      <h1>{post.title}</h1>
-      <p>{post.date}</p>
-      <p>{post.content}</p>
-      <div>
+      <Suspense fallback={<>...</>}>
         {/* @ts-expect-error Async Server Component */}
-        1つ新しい記事: <Nav Id={nextId} />
+        <Content Id={id} />
+      </Suspense>
+      <Suspense fallback={<>...</>}>
         {/* @ts-expect-error Async Server Component */}
-        <br /> 1つ古い記事: <Nav Id={prevId} />
-      </div>
+        <PrevNext Id={id} />
+      </Suspense>
     </main>
   );
+}
+
+export async function generateStaticParams() {
+  const posts = await getPostIds();
+  // ビルド時に新しいものから3つだけSSG
+  return posts.slice(0, 3).map((post) => ({
+    slug: post,
+  }));
 }
